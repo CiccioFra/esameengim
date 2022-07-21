a06a7b4e1f3a06d10eead84f09b7c37d8a92c76a
Francesco <francesco.ribatti@engimtorino.net>
2022-07-21 11:19:17 +0200

    Credenziali DB

---
+++ esameengim/src/main/java/francescoribatti/esameengim/credenzialiDB/DBConfig.java
@@ -0,0 +1,7 @@
+package francescoribatti.esameengim.credenzialiDB;
+
+public class DBConfig {
+    public static final String URL = "jdbc:mysql://localhost:3306/esameengim";
+    public static final String USER = "esameengim";
+    public static final String PW = "password";
+}
