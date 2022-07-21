91d517a6a52939699f573047ef2b17f699a6fb3c
Francesco <francesco.ribatti@engimtorino.net>
2022-07-21 09:34:47 +0200

    creazione Progetto

---
+++ esameengim/src/main/java/francescoribatti/esameengim/EsameengimApplication.java
@@ -0,0 +1,13 @@
+package francescoribatti.esameengim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsameengimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsameengimApplication.class, args);
+    }
+
+}
---
+++ esameengim/src/test/java/francescoribatti/esameengim/EsameengimApplicationTests.java
@@ -0,0 +1,13 @@
+package francescoribatti.esameengim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsameengimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
