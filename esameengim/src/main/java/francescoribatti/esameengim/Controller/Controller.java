5ad6a60e1ec843d0f251d04806dae042b5a97c7e
Francesco <francesco.ribatti@engimtorino.net>
2022-07-21 15:39:54 +0200

    iniziato il Controller

--- esameengim/src/main/java/francescoribatti/esameengim/Controller/Controller.java
+++ esameengim/src/main/java/francescoribatti/esameengim/Controller/Controller.java
@@ -1,4 +1,34 @@
 package francescoribatti.esameengim.Controller;
 
+import francescoribatti.esameengim.model.Articolo;
+import francescoribatti.esameengim.model.Ordine;
+import francescoribatti.esameengim.model.TariffaCorriere;
+import francescoribatti.esameengim.model.repository.ArticoloRepository;
+import francescoribatti.esameengim.model.repository.OrdineRepository;
+import francescoribatti.esameengim.model.repository.TariffaCorriereRepository;
+import francescoribatti.esameengim.payload.TariffaOrdiniPayload;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class Controller {
+
+    @GetMapping("/articoli")
+    public List<Articolo> articoli(){
+        return ArticoloRepository.getArticoli();
+    }
+
+    @GetMapping("/ordini")
+    public List<Ordine> ordini(){
+        return OrdineRepository.getOrdini();
+    }
+
+    @GetMapping("/tariffe")
+    public List<TariffaCorriere> tariffe(){
+        return TariffaCorriereRepository.getTariffe();
+    }
+
 }
+
