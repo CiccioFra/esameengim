6b0192d6319ef2daa4887acd96c37baa2bfc25c0
Francesco <francesco.ribatti@engimtorino.net>
2022-07-21 15:38:53 +0200

    Metodi per calcolare le tariffe degli ordini

--- esameengim/src/main/java/francescoribatti/esameengim/payload/TariffaOrdiniPayload.java
+++ esameengim/src/main/java/francescoribatti/esameengim/payload/TariffaOrdiniPayload.java
@@ -2,15 +2,16 @@ package francescoribatti.esameengim.payload;
 
 import francescoribatti.esameengim.model.Ordine;
 import francescoribatti.esameengim.model.TariffaCorriere;
+import francescoribatti.esameengim.model.repository.OrdineRepository;
 import francescoribatti.esameengim.model.repository.TariffaCorriereRepository;
 
 import java.util.List;
 
-public class Payload extends Ordine {
+public class TariffaOrdiniPayload extends Ordine {
 
     private Ordine ordine;
 
-    public Payload(int id, int numero, String data, Ordine ordine) {
+    public TariffaOrdiniPayload(int id, int numero, String data, Ordine ordine) {
         super(id, numero, data);
         this.ordine = ordine;
     }
@@ -23,7 +24,13 @@ public class Payload extends Ordine {
         this.ordine = ordine;
     }
 
-    public static TariffaCorriere tariffaConveniente(double peso, int xx){
+    /**
+     * Trova la tariffa più convenite di un corriere
+     * in base al peso dell'ordine
+     * @param peso peso complessivo dell'ordine
+     * @return  La tariffa più conveniente
+     */
+    public static TariffaCorriere tariffaConveniente(double peso){
         TariffaCorriere tariffaParsimoniosa = null;
         List<TariffaCorriere> tariffeEcoPerPeso = TariffaCorriereRepository.getTariffePerPeso(peso);
         for (TariffaCorriere tariffa : tariffeEcoPerPeso) {
@@ -35,5 +42,19 @@ public class Payload extends Ordine {
             }
         }
         return tariffaParsimoniosa;
+    }
+
+    /**
+     * Calcola la tariffa più conveniente per un ordine
+     * @param id_ordine ID dell'ordine
+     * @return tariffa più conveniente di un ordine
+     */
+    public double tariffaEconomica(int id_ordine){
+
+        double pesoOrdine = OrdineRepository.getPesoOrdine(id_ordine);
+        TariffaCorriere tariffaEconomica = TariffaOrdiniPayload.tariffaConveniente(pesoOrdine);
+        double prezzoTariffaConveniente = tariffaEconomica.getCosto();
+
+        return prezzoTariffaConveniente;
     }
 }
