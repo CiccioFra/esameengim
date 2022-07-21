22ebb5f148f5479e0705b77f4013e20174b61c6d
Francesco <francesco.ribatti@engimtorino.net>
2022-07-21 15:38:26 +0200

    Creazione Metodi per gestire gli ordini

--- esameengim/src/main/java/francescoribatti/esameengim/model/repository/OrdineRepository.java
+++ esameengim/src/main/java/francescoribatti/esameengim/model/repository/OrdineRepository.java
@@ -41,4 +41,61 @@ public class OrdineRepository {
         //restituisco l'elenco degli ordini
         return ordini;
     }
+
+    /**
+     * Crea l'oggetto Ordine con tutti i suoi metadati
+     * @param id ID dell'ordine
+     * @return Ordine
+     */
+    public static Ordine getOrdineByID(int id){
+        Ordine ordine = null;
+
+        try {
+            Connection conn = DriverManager.getConnection(URL, USER, PW);
+            PreparedStatement stmt = conn.prepareStatement("select id, numero, data FROM ordine WHERE id = ?");
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                ordine = new Ordine(
+                        rs.getInt("id"),
+                        rs.getInt("numero"),
+                        rs.getString("data")
+                );
+            }
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ordine;
+    }
+
+    /**
+     * Calcola il peso di ogni ordine
+     * @param id_ordine ID dell'ordine
+     * @return peso dell'ordine
+     */
+    public static double getPesoOrdine(int id_ordine){
+        double peso = 0;
+
+        try {
+            Connection conn = DriverManager.getConnection(URL, USER, PW);
+            PreparedStatement stmt = conn.prepareStatement("select id_articolo, quantita_articolo FROM voce WHERE id_ordine = ?");
+            stmt.setInt(1, id_ordine);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                int id_articolo = rs.getInt("id_articolo");
+                Articolo articolo = ArticoloRepository.getArticoloByID(id_articolo);
+
+                int nrArticoli = rs.getInt("quantita_articolo");
+
+                peso += articolo.getPeso() * nrArticoli;
+            }
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return peso;
+    }
 }
